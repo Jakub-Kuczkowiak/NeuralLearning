74ac0f48fa94ead16d3b026ffba7c93cfa0ce32b
Konrad Werbliński <konrad.werblinski@gmail.com>
2019-02-01 16:45:00 +0100

    Helper impovement

--- helper.py
+++ helper.py
@@ -19,6 +19,12 @@ def celeba_preprocessing(image, width, height, **kwargs):
     i = (image.size[1] - face_height) // 2
     return image.crop([j, i, j + face_width, i + face_height]).resize([width, height], Image.BILINEAR)
 
+def preprocessing(image, width, height, face_size, delta_i, delta_j):
+    face_width = face_height = face_size
+    j = (image.size[0] - face_width) // 2 + delta_j
+    i = (image.size[1] - face_height) // 2 + delta_i
+    return image.crop([j, i, j + face_width, i + face_height]).resize([width, height], Image.BILINEAR) 
+
 
 def load(image_files, mode, preprocessing_function=lambda x, **kw: x, **preprocessing_kwargs):
     data_batch = np.array(
